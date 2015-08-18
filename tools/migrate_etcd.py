3685a09a985f07293042bfad26d49f22d2e7afb9
dengqian <dengqian@foxmail.com>
2015-08-18 20:59:20 +0800

    refactoring migrate

--- tools/migrate_etcd.py
+++ tools/migrate_etcd.py
@@ -6,24 +6,38 @@ import json
 
 
 def get_node(c, key):
-    for node in c.read(key).leaves:
-        if node.dir:
-            for i in get_node(c, node.key):
-                yield i
-        else:
-            yield node.key, node.value
+    for node in c.read(key, recursive=True).leaves:
+        yield node.key, node.value
 
+def create(addr):
+    addr_host, addr_port = addr.split(":")
+    return etcd.Client(addr_host, int(addr_port))
+
+def main(src, dst):
+    source_etcd = create(src)
+    target_etcd = create(dst)
+
+    nodes = dict(get_node(source_etcd, "/skydns/com/hunantv"))
+    for k, v in nodes.iteritems():
+        if not v or k == '.wildcards':
+            continue
+        host = json.loads(v)
+        if host.get("default", None):
+            continue
+        target_etcd.set(k, json.dumps({'default': [host]}))
+
+def check(src):
+    c = create(src)
+    r = c.read('/skydns/com/hunantv', recursive=True)
+    for node in r.get_subtree():
+        if node.dir:
+            continue
+        v = json.loads(node.value)
+        if v.get('default'):
+            continue
+        print node.key, v
 
 if __name__=="__main__":
-    source_host = raw_input("please enter source host and port. ")
-    target_host = raw_input("please enter target host and port. ")
-    src_host, src_ip = source_host.split(":")
-    tgt_host, tgt_ip = target_host.split(":")
-    source_etcd = etcd.Client(src_host, int(src_ip))
-    target_etcd = etcd.Client(tgt_host, int(tgt_ip))
-
-    nodes = dict(get_node(source_etcd, "/skydns/com"))
-    for k, v in nodes.iteritems():
-        host = json.loads(v)
-        if host.get("default") is None:
-            target_etcd.set(k, json.dumps({'default': [host]}))
+    dst = src = '127.0.0.1:4001'
+    main(src, dst)
+    check(src)
